cd83099096606ef31a6c8205eced3eb7ead89ca5
zoubuchuxiezi <1042523916@qq.com>
2018-08-27 23:52:07 +0800

    修改打war包

--- src/main/java/com/article/ArticleApplication.java
+++ src/main/java/com/article/ArticleApplication.java
@@ -3,12 +3,18 @@ package com.article;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.article.dao")
-public class ArticleApplication {
+public class ArticleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArticleApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
